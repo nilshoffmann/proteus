73260e96fff625c0e1eee69c5694eba76b46ffe6
Konstantin Otte <kotte@users.sf.net>
2011-03-07 14:25:20 +0000

    Spot

---
+++ Proteomik API/src/de/unibielefeld/gi/kotte/laborprogramm/proteomik/api/SpotStatus.java
@@ -0,0 +1,11 @@
+package de.unibielefeld.gi.kotte.laborprogramm.proteomik.api;
+
+/**
+ *
+ * @author kotte
+ */
+public enum SpotStatus {
+    DETECTED,
+    PICKED,
+    IDENTIFIED;
+}
--- Proteomik API/src/de/unibielefeld/gi/kotte/laborprogramm/proteomik/spi/Spot.java
+++ Proteomik API/src/de/unibielefeld/gi/kotte/laborprogramm/proteomik/spi/Spot.java
@@ -1,6 +1,8 @@
 package de.unibielefeld.gi.kotte.laborprogramm.proteomik.spi;
 
+import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.IGel;
 import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.ISpot;
+import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.SpotStatus;
 
 /**
  *
@@ -8,5 +10,59 @@ import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.ISpot;
  */
 @org.openide.util.lookup.ServiceProvider(service=ISpot.class)
 public class Spot implements ISpot{
+
+    SpotStatus status;
+    IGel parent;
+    int posX, posY;
+    String label;
+    boolean labelDisplayed;
+
+    public String getLabel() {
+        return label;
+    }
+
+    public boolean isLabelDisplayed() {
+        return labelDisplayed;
+    }
+
+    public IGel getParent() {
+        return parent;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public SpotStatus getStatus() {
+        return status;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setLabelDisplayed(boolean labelDisplayed) {
+        this.labelDisplayed = labelDisplayed;
+    }
+
+    public void setParent(IGel parent) {
+        this.parent = parent;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+
+    public void setStatus(SpotStatus status) {
+        this.status = status;
+    }
 
 }

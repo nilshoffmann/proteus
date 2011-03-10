99f7f7ca04da9682816eda5face9ea6e629af362
Konstantin Otte <kotte@users.sf.net>
2011-03-10 16:37:13 +0000

    ...

---
+++ PlateViewer/src/de/unibielefeld/gi/kotte/laborprogramm/plateViewer/PlatePanel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package de.unibielefeld.gi.kotte.laborprogramm.plateViewer;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author kotte
+ */
+public class PlatePanel extends JPanel {
+
+    class WellAction extends AbstractAction {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+    }
+}

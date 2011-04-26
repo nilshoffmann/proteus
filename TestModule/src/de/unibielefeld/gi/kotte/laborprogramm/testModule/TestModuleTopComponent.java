ab87e274abe0dc06e84a9432c18dd5270e960cf3
Konstantin Otte <kotte@users.sf.net>
2011-04-26 16:49:40 +0000

    Testmodul angepasst

--- TestModule/src/de/unibielefeld/gi/kotte/laborprogramm/testModule/TestModuleTopComponent.java
+++ TestModule/src/de/unibielefeld/gi/kotte/laborprogramm/testModule/TestModuleTopComponent.java
@@ -1,6 +1,9 @@
 package de.unibielefeld.gi.kotte.laborprogramm.testModule;
 
+import de.unibielefeld.gi.kotte.laborprogramm.plateViewer.Plate384Panel;
 import de.unibielefeld.gi.kotte.laborprogramm.plateViewer.Plate96Panel;
+import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.plate384.IPlate384;
+import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.plate384.IPlate384Factory;
 import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.plate96.IPlate96;
 import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.plate96.IPlate96Factory;
 import java.util.logging.Logger;
@@ -41,12 +44,20 @@ public final class TestModuleTopComponent extends TopComponent {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        plateViewerTestButton = new javax.swing.JButton();
+        plate96TestButton = new javax.swing.JButton();
+        plate384TestButton = new javax.swing.JButton();
 
-        org.openide.awt.Mnemonics.setLocalizedText(plateViewerTestButton, org.openide.util.NbBundle.getMessage(TestModuleTopComponent.class, "TestModuleTopComponent.plateViewerTestButton.text")); // NOI18N
-        plateViewerTestButton.addActionListener(new java.awt.event.ActionListener() {
+        org.openide.awt.Mnemonics.setLocalizedText(plate96TestButton, org.openide.util.NbBundle.getMessage(TestModuleTopComponent.class, "TestModuleTopComponent.plate96TestButton.text")); // NOI18N
+        plate96TestButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                plateViewerTestButtonActionPerformed(evt);
+                plate96TestButtonActionPerformed(evt);
+            }
+        });
+
+        org.openide.awt.Mnemonics.setLocalizedText(plate384TestButton, org.openide.util.NbBundle.getMessage(TestModuleTopComponent.class, "TestModuleTopComponent.plate384TestButton.text")); // NOI18N
+        plate384TestButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                plate384TestButtonActionPerformed(evt);
             }
         });
 
@@ -56,29 +67,43 @@ public final class TestModuleTopComponent extends TopComponent {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(plateViewerTestButton)
-                .addContainerGap(271, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(plate96TestButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(plate384TestButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(261, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(plateViewerTestButton)
-                .addContainerGap(266, Short.MAX_VALUE))
+                .addComponent(plate96TestButton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(plate384TestButton)
+                .addContainerGap(237, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void plateViewerTestButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_plateViewerTestButtonActionPerformed
+    private void plate96TestButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_plate96TestButtonActionPerformed
         IPlate96 plate = Lookup.getDefault().lookup(IPlate96Factory.class).createPlate96();
         JPanel panel = new Plate96Panel(plate);
         JDialog dialog = new JDialog();
         dialog.add(panel);
         dialog.pack();
         dialog.setVisible(true);
-    }//GEN-LAST:event_plateViewerTestButtonActionPerformed
+    }//GEN-LAST:event_plate96TestButtonActionPerformed
+
+    private void plate384TestButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_plate384TestButtonActionPerformed
+        IPlate384 plate = Lookup.getDefault().lookup(IPlate384Factory.class).createPlate384();
+        JPanel panel = new Plate384Panel(plate);
+        JDialog dialog = new JDialog();
+        dialog.add(panel);
+        dialog.pack();
+        dialog.setVisible(true);
+    }//GEN-LAST:event_plate384TestButtonActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton plateViewerTestButton;
+    private javax.swing.JButton plate384TestButton;
+    private javax.swing.JButton plate96TestButton;
     // End of variables declaration//GEN-END:variables
     /**
      * Gets default instance. Do not use directly: reserved for *.settings files only,

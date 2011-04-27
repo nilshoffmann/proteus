7f4246c5592e545ba3f4ff4b475ebb1fc71fe506
Konstantin Otte <kotte@users.sf.net>
2011-04-27 13:32:19 +0000

    aufrauemen...

--- Proteomik Project/src/de/unibielefeld/gi/kotte/laborprogramm/project/factory/spi/ProteomikProjectFactory.java
+++ Proteomik Project/src/de/unibielefeld/gi/kotte/laborprogramm/project/factory/spi/ProteomikProjectFactory.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package de.unibielefeld.gi.kotte.laborprogramm.project.factory.spi;
 
 import de.unibielefeld.gi.kotte.laborprogramm.project.factory.api.IProteomicProject;

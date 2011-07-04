7f794be4dfa7a58f5ee7828374ca38af894bb3f8
Nils Hoffmann <nilshoffmann@users.sf.net>
2011-07-04 09:42:23 +0000

    Modified interface to accept explicit setting of classloader.

--- DatabaseServices/src/net/sf/maltcms/chromaui/db/api/ICrudProviderFactory.java
+++ DatabaseServices/src/net/sf/maltcms/chromaui/db/api/ICrudProviderFactory.java
@@ -12,4 +12,5 @@ import java.net.URL;
  */
 public interface ICrudProviderFactory {
     ICrudProvider getCrudProvider(URL databaseLocation, ICredentials ic, ClassLoader cl);
+    ICrudProvider getCrudProvider(URL databaseLocation, ICredentials ic);
 }

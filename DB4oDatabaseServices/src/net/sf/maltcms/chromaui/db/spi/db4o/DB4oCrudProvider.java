04669d994ee1aa7d465fab626ca5b74da6faaf2b
Nils Hoffmann <nilshoffmann@users.sf.net>
2011-07-04 15:38:08 +0000

    ExcelReader now throws an exception if gel names can  not be matched to column/gel names in xlsx file.

--- DB4oDatabaseServices/src/net/sf/maltcms/chromaui/db/spi/db4o/DB4oCrudProvider.java
+++ DB4oDatabaseServices/src/net/sf/maltcms/chromaui/db/spi/db4o/DB4oCrudProvider.java
@@ -58,7 +58,7 @@ public final class DB4oCrudProvider implements ICrudProvider {
         if (eoc == null) {
             System.out.println("Opening ObjectContainer at " + projectDBLocation.getAbsolutePath());
             EmbeddedConfiguration ec = com.db4o.Db4oEmbedded.newConfiguration();
-            ec.common().activationDepth(Integer.MAX_VALUE);
+            ec.common().activationDepth(10);
             ec.common().reflectWith(new JdkReflector(this.domainClassLoader));
 //            ec.common().add(new TransparentActivationSupport());
             eoc = Db4oEmbedded.openFile(ec, projectDBLocation.getAbsolutePath());

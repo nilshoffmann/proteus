04d353902b2e69f36044731b5de3d53eef91f917
Konstantin Otte <kotte@users.sf.net>
2011-07-21 10:41:52 +0000

    Statusauswahl bei den Well96Buttons erste Version (noch nicht ganz funktional)

--- Plate96 Viewer/src/de/unibielefeld/gi/kotte/laborprogramm/plate96Viewer/Plate96Panel.java
+++ Plate96 Viewer/src/de/unibielefeld/gi/kotte/laborprogramm/plate96Viewer/Plate96Panel.java
@@ -15,42 +15,26 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 
 /**
+ * GUI Panel that visually represents a IPlate96. Offers Well96Buttons to manipulate plate wells.
  *
  * @author kotte
  */
 public class Plate96Panel extends JPanel {
 
-    private class Well96Action extends AbstractAction {
-
-        IWell96 well;
-        JButton button;
-
-        public Well96Action(IWell96 well, JButton button) {
-            this.well = well;
-            this.button = button;
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            //TODO aktuellen Well im LookUp anmelden
-            well.setStatus(Well96Status.FILLED);
-            button.setIcon(wellOkay);
-        }
-    }
-    private Icon wellEmpty;
-    private Icon wellError;
-    private Icon wellOkay;
+//    private Icon wellEmpty;
+//    private Icon wellError;
+//    private Icon wellOkay;
     //private final static String resourcePath = "resources/";
     //de/unibielefeld/gi/kotte/laborprogramm/plateViewer/
 
     public Plate96Panel(IPlate96 plate) {
         //Icons to display wells on the plate
-        URL emptyURL = Plate96Panel.class.getResource("resources/Well01.jpg");
-        URL errorURL = Plate96Panel.class.getResource("resources/Well02.jpg");
-        URL  okayURL = Plate96Panel.class.getResource("resources/Well03.jpg");
-        wellEmpty = new ImageIcon(emptyURL);//resourcePath + "Well01.jpg");
-        wellError = new ImageIcon(errorURL);//resourcePath + "Well02.jpg");
-        wellOkay  = new ImageIcon(okayURL); //resourcePath + "Well03.jpg");
+//        URL emptyURL = Plate96Panel.class.getResource("resources/Well01.jpg");
+//        URL errorURL = Plate96Panel.class.getResource("resources/Well02.jpg");
+//        URL  okayURL = Plate96Panel.class.getResource("resources/Well03.jpg");
+//        wellEmpty = new ImageIcon(emptyURL);//resourcePath + "Well01.jpg");
+//        wellError = new ImageIcon(errorURL);//resourcePath + "Well02.jpg");
+//        wellOkay  = new ImageIcon(okayURL); //resourcePath + "Well03.jpg");
 
         final int x = plate.getXdimension();
         final int y = plate.getYdimension();
@@ -71,27 +55,29 @@ public class Plate96Panel extends JPanel {
             jl.setHorizontalAlignment(JLabel.RIGHT);
             add(jl); //add leftmost column label
 
-            Dimension dim16 = new Dimension(16, 16);
+            //Dimension dim16 = new Dimension(16, 16);
             for (int i = 1; i <= x; i++) {
                 //create button for the current well on the plate grid
-                IWell96 currentWell = plate.getWell(c, i);
-                JButton button = new JButton();
-                button.setAction(new Well96Action(currentWell, button));
-                button.setName(currentWell.getWellPosition());
-                button.setBorderPainted(false);
-                button.setContentAreaFilled(false);
-                button.setPreferredSize(dim16);
-                button.setMinimumSize(dim16);
-                switch (currentWell.getStatus()) {
-                    case ERROR:
-                        button.setIcon(wellError);
-                        break;
-                    case FILLED:
-                        button.setIcon(wellOkay);
-                        break;
-                    default:
-                        button.setIcon(wellEmpty);
-                }
+                JButton button = new Well96Button(plate.getWell(c, i));
+
+                //IWell96 currentWell = plate.getWell(c, i);
+                //JButton button = new JButton();
+                //button.setAction(new Well96Action());
+                //button.setName(currentWell.getWellPosition());
+                //button.setBorderPainted(false);
+                //button.setContentAreaFilled(false);
+                //button.setPreferredSize(dim16);
+                //button.setMinimumSize(dim16);
+//                switch (currentWell.getStatus()) {
+//                    case ERROR:
+//                        button.setIcon(wellError);
+//                        break;
+//                    case FILLED:
+//                        button.setIcon(wellOkay);
+//                        break;
+//                    default:
+//                        button.setIcon(wellEmpty);
+//                }
                 add(button); //add main grid area button
             }
         }
--- Plate96 Viewer/src/de/unibielefeld/gi/kotte/laborprogramm/plate96Viewer/Well96Button.java
+++ Plate96 Viewer/src/de/unibielefeld/gi/kotte/laborprogramm/plate96Viewer/Well96Button.java
@@ -1,38 +1,113 @@
 package de.unibielefeld.gi.kotte.laborprogramm.plate96Viewer;
 
+import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.gel.ISpot;
 import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.plate96.IWell96;
 import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.plate96.Well96Status;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 /**
+ * Button to be placed on a Plate96Panel. Represents a IWell96 and offers actions to set the wells status.
  *
- * @author hoffmann
+ * @author hoffmann, kotte
  */
 public class Well96Button extends JButton {
 
-    private IWell96 well96 = null;
+    private IWell96 well = null;
 
-    public IWell96 getWell96() {
-        return well96;
+    private class Well96Action extends AbstractAction {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JDialog dia = new Well96StatusSelectDialog(well.getStatus());
+            dia.setVisible(true);
+        }
     }
 
-    public void setWell96(IWell96 well96) {
-        this.well96 = well96;
+    private class Well96StatusSelectDialog extends JDialog {
+
+        public Well96StatusSelectDialog(Well96Status status) {
+            JPanel panel = new JPanel();
+            panel.add(new JLabel("choose status:"));
+            ButtonGroup group = new ButtonGroup();
+            for (Well96Status s : Well96Status.values()) {
+                JRadioButton jrb = new JRadioButton(s.toString());
+                if (s == status) {
+                    jrb.setSelected(true);
+                }
+                jrb.setAction(new Well96StatusSelectRadioButtonAction(s));
+                group.add(jrb);
+                panel.add(jrb);
+            }
+            add(panel);
+        }
+    }
+
+    private class Well96StatusSelectRadioButtonAction extends AbstractAction {
+
+        private Well96Status status;
+
+        public Well96StatusSelectRadioButtonAction(Well96Status status) {
+            this.status = status;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            selectStatus(status);
+        }
+    }
+
+    public Well96Button(IWell96 well) {
+        this.well = well;
+        setAction(new Well96Action());
+        setName(well.getWellPosition());
+        setBorderPainted(false);
+        setContentAreaFilled(false);
+    }
+
+//    public IWell96 getWell96() {
+//        return well96;
+//    }
+//
+//    public void setWell96(IWell96 well96) {
+//        this.well96 = well96;
+//    }
+
+    private void selectStatus(Well96Status status) {
+        if(!well.getStatus().equals(status)){
+            if(status.equals(Well96Status.FILLED)) {
+                //TODO get Spot from lookup
+                //spot.setWell(well96)
+                //well96.setSpot(spot);
+            }
+            if(status.equals(Well96Status.EMPTY) && well.getStatus().equals(Well96Status.FILLED)) {
+                ISpot spot = well.getSpot();
+                well.setSpot(null);
+                spot.setWell(null);
+            }
+            well.setStatus(status);
+            repaint();
+        }
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2 = (Graphics2D)g;
+        Graphics2D g2 = (Graphics2D) g;
         Color originalColor = g2.getColor();
-        g2.setColor(Well96Status.getColor(well96.getStatus()));
-        int width = getWidth()-(getInsets().left+getInsets().right);
-        int height = getHeight()-(getInsets().top+getInsets().bottom);
+        g2.setColor(well.getStatus().getColor());
+        int width = getWidth() - (getInsets().left + getInsets().right);
+        int height = getHeight() - (getInsets().top + getInsets().bottom);
         g2.fillOval(getInsets().left, getInsets().top, width, height);
         g2.setColor(originalColor);
     }
-
 }

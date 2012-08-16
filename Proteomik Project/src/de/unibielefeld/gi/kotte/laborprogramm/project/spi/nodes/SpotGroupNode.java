f5dc06013fe122edabc54c7bd1c6d3db99313a81
Konstantin Otte <kotte@users.sf.net>
2012-08-16 12:05:53 +0000

    ShortDescription für SpotGroupNodes

--- Proteomik Project/src/de/unibielefeld/gi/kotte/laborprogramm/project/spi/nodes/SpotGroupNode.java
+++ Proteomik Project/src/de/unibielefeld/gi/kotte/laborprogramm/project/spi/nodes/SpotGroupNode.java
@@ -3,7 +3,13 @@ package de.unibielefeld.gi.kotte.laborprogramm.project.spi.nodes;
 import de.unibielefeld.gi.kotte.laborprogramm.project.api.IProteomicProject;
 import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.IProject;
 import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.gel.ISpot;
+import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.gel.SpotStatus;
 import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.gel.group.ISpotGroup;
+import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.identification.IIdentification;
+import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.identification.IIdentificationMethod;
+import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.plate384.IWell384;
+import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.plate96.IWell96;
+import de.unibielefeld.gi.kotte.laborprogramm.proteomik.api.plate96.Well96Status;
 import java.awt.Image;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
@@ -112,6 +118,36 @@ public class SpotGroupNode extends AbstractNode implements PropertyChangeListene
         }else{
             spotGroupLabel.append("#");
             spotGroupLabel.append(getLookup().lookup(ISpotGroup.class).getNumber());
+        }
+        return spotGroupLabel.toString();
+    }
+
+    @Override
+    public String getShortDescription() {
+        StringBuilder spotGroupLabel = new StringBuilder();
+        ISpotGroup group = getLookup().lookup(ISpotGroup.class);
+        boolean filled = false;
+        for (ISpot spot : group.getSpots()) {
+            if (spot.getStatus() == SpotStatus.PICKED) {
+                IWell96 well96 = spot.getWell();
+                if (well96.getStatus() == Well96Status.PROCESSED) {
+                    for (IWell384 well384 : well96.get384Wells()) {
+                        for (IIdentificationMethod method : well384.getIdentification().getMethods()) {
+                            for (IIdentification ident : method.getIdentifications()) {
+                                if(filled) {
+                                    spotGroupLabel.append(", ");
+                                }
+//                                spotGroupLabel.append(ident.getAbbreviation());
+                                spotGroupLabel.append(ident.getName());
+                                filled = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (!filled) {
+            spotGroupLabel.append("unidentified");
         }
         return spotGroupLabel.toString();
     }

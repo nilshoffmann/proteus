6edb1e32773ff98af01e1488751fe606883e5f12
Konstantin Otte <kotte@users.sf.net>
2012-11-15 14:33:14 +0000

    aktueller Zwischenstand des Pathway Managers

--- Pathway Manager/src/de/unibielefeld/gi/kotte/laborprogramm/pathways/docs/package-info.java
+++
@@ -1,8 +0,0 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-@HelpSetRegistration(helpSet = "pathways-hs.xml", position = 3115)
-package de.unibielefeld.gi.kotte.laborprogramm.pathways.docs;
-
-import org.netbeans.api.javahelp.HelpSetRegistration;
